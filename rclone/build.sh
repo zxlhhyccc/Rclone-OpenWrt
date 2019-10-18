26442af028659d993da490fd05f6d8fd57946cc7
elonh <846131012@qq.com>
2019-10-18 17:30:14 +0800

    ⚡️ improve performance of build script
    disable unused go build command line
    rename mipsle => mipsel

--- rclone/build.sh
+++ rclone/build.sh
@@ -31,11 +31,11 @@ upx --lzma -o ${comp_dir}/amd64 ${build_dir}/amd64
 GOARCH=386 GO386=387 go build -a -ldflags '-s -w' -o ${build_dir}/i386_387 $main_file
 upx --lzma -o ${comp_dir}/i386_387 ${build_dir}/i386_387
 
-GOARCH=386 GO386=sse2 go build -a -ldflags '-s -w' -o ${build_dir}/i386_sse2 $main_file
-upx --lzma -o ${comp_dir}/i386_sse2 ${build_dir}/i386_sse2
+# GOARCH=386 GO386=sse2 go build -a -ldflags '-s -w' -o ${build_dir}/i386_sse2 $main_file
+# upx --lzma -o ${comp_dir}/i386_sse2 ${build_dir}/i386_sse2
 
-GOARCH=arm GOARM=5 go build -a -ldflags '-s -w' -o ${build_dir}/arm_5 $main_file
-upx --lzma -o ${comp_dir}/arm_5 ${build_dir}/arm_5
+# GOARCH=arm GOARM=5 go build -a -ldflags '-s -w' -o ${build_dir}/arm_5 $main_file
+# upx --lzma -o ${comp_dir}/arm_5 ${build_dir}/arm_5
 
 GOARCH=arm GOARM=6 go build -a -ldflags '-s -w' -o ${build_dir}/arm_6 $main_file
 upx --lzma -o ${comp_dir}/arm_6 ${build_dir}/arm_6
@@ -46,13 +46,13 @@ upx --lzma -o ${comp_dir}/arm_7 ${build_dir}/arm_7
 GOARCH=arm64 go build -a -ldflags '-s -w' -o ${build_dir}/arm64 $main_file
 upx --lzma -o ${comp_dir}/arm64 ${build_dir}/arm64
 
-GOARCH=ppc64 GOPPC64=power8 go build -a -ldflags '-s -w' -o ${build_dir}/ppc64_power8 $main_file
-# upx --lzma -o ${comp_dir}/ppc64_power8 ${build_dir}/ppc64_power8
-cp ${build_dir}/ppc64_power8 ${comp_dir}/ppc64_power8
+# GOARCH=ppc64 GOPPC64=power8 go build -a -ldflags '-s -w' -o ${build_dir}/ppc64_power8 $main_file
+# # upx --lzma -o ${comp_dir}/ppc64_power8 ${build_dir}/ppc64_power8
+# cp ${build_dir}/ppc64_power8 ${comp_dir}/ppc64_power8
 
-GOARCH=ppc64 GOPPC64=power9 go build -a -ldflags '-s -w' -o ${build_dir}/ppc64_power9 $main_file
-# upx --lzma -o ${comp_dir}/ppc64_power9 ${build_dir}/ppc64_power9
-cp ${build_dir}/ppc64_power9 ${comp_dir}/ppc64_power9
+# GOARCH=ppc64 GOPPC64=power9 go build -a -ldflags '-s -w' -o ${build_dir}/ppc64_power9 $main_file
+# # upx --lzma -o ${comp_dir}/ppc64_power9 ${build_dir}/ppc64_power9
+# cp ${build_dir}/ppc64_power9 ${comp_dir}/ppc64_power9
 
 # GOARCH=ppc64le GOPPC64=power8 go build -a -ldflags '-s -w' -o ${build_dir}/ppc64le_power8 $main_file
 # upx --lzma -o ${comp_dir}/ppc64le_power8 ${build_dir}/ppc64le_power8
@@ -66,28 +66,28 @@ upx --lzma -o ${comp_dir}/mips_hardfloat ${build_dir}/mips_hardfloat
 GOARCH=mips GOMIPS=softfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips_softfloat $main_file
 upx --lzma -o ${comp_dir}/mips_softfloat ${build_dir}/mips_softfloat
 
-GOARCH=mipsle GOMIPS=hardfloat go build -a -ldflags '-s -w' -o ${build_dir}/mipsle_hardfloat $main_file
-upx --lzma -o ${comp_dir}/mipsle_hardfloat ${build_dir}/mipsle_hardfloat
+GOARCH=mipsle GOMIPS=hardfloat go build -a -ldflags '-s -w' -o ${build_dir}/mipsel_hardfloat $main_file
+upx --lzma -o ${comp_dir}/mipsel_hardfloat ${build_dir}/mipsel_hardfloat
 
-GOARCH=mipsle GOMIPS=softfloat go build -a -ldflags '-s -w' -o ${build_dir}/mipsle_softfloat $main_file
-upx --lzma -o ${comp_dir}/mipsle_softfloat ${build_dir}/mipsle_softfloat
+GOARCH=mipsle GOMIPS=softfloat go build -a -ldflags '-s -w' -o ${build_dir}/mipsel_softfloat $main_file
+upx --lzma -o ${comp_dir}/mipsel_softfloat ${build_dir}/mipsel_softfloat
 
-GOARCH=mips64 GOMIPS64=hardfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64_hardfloat $main_file
-# upx --lzma -o ${comp_dir}/mips64_hardfloat ${build_dir}/mips64_hardfloat
-cp ${build_dir}/mips64_hardfloat ${comp_dir}/mips64_hardfloat
+# GOARCH=mips64 GOMIPS64=hardfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64_hardfloat $main_file
+# # upx --lzma -o ${comp_dir}/mips64_hardfloat ${build_dir}/mips64_hardfloat
+# cp ${build_dir}/mips64_hardfloat ${comp_dir}/mips64_hardfloat
 
-GOARCH=mips64 GOMIPS64=softfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64_softfloat $main_file
-# upx --lzma -o ${comp_dir}/mips64_softfloat ${build_dir}/mips64_softfloat
-cp ${build_dir}/mips64_softfloat ${comp_dir}/mips64_softfloat
+# GOARCH=mips64 GOMIPS64=softfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64_softfloat $main_file
+# # upx --lzma -o ${comp_dir}/mips64_softfloat ${build_dir}/mips64_softfloat
+# cp ${build_dir}/mips64_softfloat ${comp_dir}/mips64_softfloat
 
-GOARCH=mips64le GOMIPS64=hardfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64le_hardfloat $main_file
-# upx --lzma -o ${comp_dir}/mips64le_hardfloat ${build_dir}/mips64le_hardfloat
-cp ${build_dir}/mips64le_hardfloat ${comp_dir}/mips64le_hardfloat
+# GOARCH=mips64le GOMIPS64=hardfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64el_hardfloat $main_file
+# # upx --lzma -o ${comp_dir}/mips64el_hardfloat ${build_dir}/mips64el_hardfloat
+# cp ${build_dir}/mips64el_hardfloat ${comp_dir}/mips64el_hardfloat
 
-GOARCH=mips64le GOMIPS64=softfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64le_softfloat $main_file
-# upx --lzma -o ${comp_dir}/mips64le_softfloat ${build_dir}/mips64le_softfloat
-cp ${build_dir}/mips64le_softfloat ${comp_dir}/mips64le_softfloat
+# GOARCH=mips64le GOMIPS64=softfloat go build -a -ldflags '-s -w' -o ${build_dir}/mips64el_softfloat $main_file
+# # upx --lzma -o ${comp_dir}/mips64el_softfloat ${build_dir}/mips64el_softfloat
+# cp ${build_dir}/mips64el_softfloat ${comp_dir}/mips64el_softfloat
 
-GOARCH=s390x go build -a -ldflags '-s -w' -o ${build_dir}/s390x $main_file
-# upx --lzma -o ${comp_dir}/s390x ${build_dir}/s390x
-cp ${build_dir}/s390x ${comp_dir}/s390x
+# GOARCH=s390x go build -a -ldflags '-s -w' -o ${build_dir}/s390x $main_file
+# # upx --lzma -o ${comp_dir}/s390x ${build_dir}/s390x
+# cp ${build_dir}/s390x ${comp_dir}/s390x
